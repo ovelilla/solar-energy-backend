c0a68e2008b87709f7ef57d4c787f2d1a7161b09
Oscar <o.velillaa@gmail.com>
2023-01-23 06:31:51 +0100

    updates

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-// import connectDB from "./config/db.js";
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import proposalRoutes from "./routes/proposalRoutes.js";
 
@@ -13,14 +13,14 @@ app.use(express.json());
 
 dotenv.config();
 
-// await connectDB();
-mongoose.set("strictQuery", true);
-mongoose.connect("mongodb+srv://oscarva:i68q1BM4i9KRVJLT@cluster0.xdfw5ec.mongodb.net/solar?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+await connectDB();
+// mongoose.set("strictQuery", true);
+// mongoose.connect("mongodb+srv://oscarva:i68q1BM4i9KRVJLT@cluster0.xdfw5ec.mongodb.net/solar?retryWrites=true&w=majority", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
 
-// const whiteList = ["http://localhost:5173", "http://localhost:5174"];
+
 const whiteList = [process.env.LANDING_URL, process.env.CRM_URL];
 
 const corsOptions = {
